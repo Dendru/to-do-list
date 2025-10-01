c5e0e41e3f25f11b339cc510a4b9c6ae9bb42197
Denis Dobrynin <denis.dobrynin09@gmail.com>
2025-10-01 16:00:59 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <h2 class="title">Эта неделя</h2>
+    <p class="month-and-year">Октярбь 2025</p>
+    <div class="week">
+    </div>
+    <div class="day-of-week">
+      <p class="name-of-day">Пятница</p>
+      <p class="day">10</p>
+    </div>
+    <div class="tasks-block">
+      <ul>
+        <li class="task">
+          <p class="task__name">Сделать работу по дому</p>
+          <p class="task__date">10.10.2025</p>
+        </li>
+      </ul>
+    </div>
+    <button class="add-button">+</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TasksPage.vue
@@ -0,0 +1,37 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Сделать работу по дому",
+          description: "Пропылесосить квартиру",
+          date: "10.10.2025",
+          completed: true,
+          checked: false,
+        },
+        {
+          title: "Написать пет проект для фронтенда",
+          description: "",
+          date: "10.10.2025",
+          completed: false,
+          checked: false,
+        },
+        {
+          title: "Написать второй пет проект для фронтенда",
+          description:
+            "Хочу сделать проект, который будет не стыдно представить на собеседовании и показать HR ",
+          date: "10.10.2025",
+          completed: false,
+          checked: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
