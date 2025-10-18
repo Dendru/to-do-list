65e28cf1b6a4824f5258edc178d91eb80fcae7f5
Denis Dobrynin <denis.dobrynin09@gmail.com>
2025-10-18 15:33:25 +0300

    I configured the reactive display of the year and month, as well as the date and day of the week on the week bar. I configured the calendar and task selection for a specific day. I configured the display of informative text when a date is not selected and when there are no tasks.

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,45 @@
   <div class="app">
     <h2 class="title">Эта неделя</h2>
     <p class="month-and-year">{{ monthYear }}</p>
-    <div class="week">
+    <div class="week" @click="selectDayAndOpenCalendar">
       <div
         v-for="day in week"
         :key="day.fullDate"
-        :class="{ today: isToday(day.fullDate) }"
-        @click="selectDayAndOpenCalendar(day.fullDate)"
+        :class="{ 'checked-day': isCheckedDay(day.fullDate) }"
       >
         <span class="day-name">{{ day.dayName }}</span>
         <span class="day-number">{{ day.dayNumber }}</span>
       </div>
     </div>
-      <calendar-picker v-if="calendarOpen" class="calendar"></calendar-picker>
-      <div class="tasks-block">
-        <ul>
-          <li
-            v-for="task in tasks"
-            :key="task.id"
-            class="task"
-            :class="{
-              completed: task.completed,
-              selected: selectedTaskId === task.id,
-            }"
-            @click="selectedTask(task.id)"
-          >
-            <p class="task__name">{{ task.title }}</p>
-            <p class="task__description" v-if="selectedTaskId === task.id">
-              {{ task.description }}
-            </p>
-            <p class="task__date">{{ task.date }}</p>
-          </li>
-        </ul>
-      </div>
-      <button class="add-button">+</button>
+    <calendar-picker
+      v-if="calendarOpen"
+      class="calendar"
+      @date-selected="handleDateSelect"
+    ></calendar-picker>
+
+    <p v-if="!selectedDay">Все задачи</p>
+    <div class="tasks-block">
+      <ul v-if="filteredTasks.length > 0">
+        <li
+          v-for="task in filteredTasks"
+          :key="task.id"
+          class="task"
+          :class="{
+            completed: task.completed,
+            selected: selectedTaskId === task.id,
+          }"
+          @click="selectedTask(task.id)"
+        >
+          <p class="task__name">{{ task.title }}</p>
+          <p class="task__description" v-if="selectedTaskId === task.id">
+            {{ task.description }}
+          </p>
+          <p class="task__date">{{ task.date }}</p>
+        </li>
+      </ul>
+      <p v-else>В этот день нет задач</p>
+    </div>
+    <button class="add-button">+</button>
   </div>
 </template>
 
@@ -44,6 +50,14 @@ import CalendarPicker from "./components/CalendarPicker.vue";
 
 const calendarOpen = ref(false);
 const date = ref(new Date());
+const selectedDay = ref(null);
+const week = ref([]);
+const selectedTaskId = ref(null);
+const currentMonth = ref(date.value.getMonth());
+const currentYear = ref(date.value.getFullYear());
+
+week.value = getCurrentWeek();
+
 const months = [
   "Январь",
   "Февраль",
@@ -58,26 +72,20 @@ const months = [
   "Ноябрь",
   "Декабрь",
 ];
-const monthYear = computed(() => {
-  return `${months[date.value.getMonth()]} ${date.value.getFullYear()}`;
-});
-const selectedDay = ref(null);
-const week = ref([]);
-const selectedTaskId = ref(null);
 const tasks = ref([
   {
     id: 1,
     title: "Сделать работу по дому",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo laborum quo sequi blanditiis porro quas nam nobis hic, fugit minima, sed pariatur eaque, consectetur assumenda soluta vitae incidunt doloribus in. Beatae suscipit dolor ea vitae temporibus doloribus inventore autem cum nihil ducimus quas et, perferendis eveniet unde similique. Neque nisi recusandae et eligendi, doloribus repellendus maxime dolorem animi quas provident illum nobis vero! Cum provident adipisci maxime laborum rerum ad et libero necessitatibus quam error distinctio voluptatibus repellat, velit voluptatem expedita unde suscipit ut blanditiis sed officia vero. Illum?",
-    date: "10.10.2025",
+    date: "18.10.2025",
     completed: true,
   },
   {
     id: 2,
     title: "Сделать пет проект для фронтенда",
     description: "",
-    date: "10.10.2025",
+    date: "19.10.2025",
     completed: false,
   },
   {
@@ -85,10 +93,15 @@ const tasks = ref([
     title: "Написать второй пет проект для фронтенда",
     description:
       "Хочу сделать проект, который будет не стыдно представить на собеседовании и показать на HR",
-    date: "10.10.2025",
+    date: "20.10.2025",
     completed: false,
   },
 ]);
+
+const monthYear = computed(() => {
+  return `${months[currentMonth.value]} ${currentYear.value}`;
+});
+
 const selectedTask = (id) => {
   if (selectedTaskId.value === id) {
     selectedTaskId.value = null;
@@ -97,15 +110,14 @@ const selectedTask = (id) => {
   }
 };
 
-function getCurrentWeek() {
-  const today = new Date();
-  const dayOfWeek = today.getDay();
+function getCurrentWeek(baseDate = new Date()) {
+  const dayOfWeek = baseDate.getDay();
   const mondayOffset = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
 
   const arr = [];
   for (let i = 0; i < 7; i++) {
-    const d = new Date(today);
-    d.setDate(today.getDate() + mondayOffset + i);
+    const d = new Date(baseDate);
+    d.setDate(baseDate.getDate() + mondayOffset + i);
     arr.push({
       dayName: d.toLocaleDateString("ru-RU", { weekday: "short" }),
       dayNumber: d.getDate(),
@@ -115,17 +127,36 @@ function getCurrentWeek() {
   return arr;
 }
 
-week.value = getCurrentWeek();
-
-function isToday(date) {
-  const today = new Date();
-  return date.toDateString() === today.toDateString();
+function isCheckedDay(day) {
+  const checkedDay = selectedDay.value
+    ? new Date(selectedDay.value)
+    : new Date();
+  return day.toDateString() === checkedDay.toDateString();
 }
 
-function selectDayAndOpenCalendar(date) {
-  selectedDay.value = date;
+function selectDayAndOpenCalendar() {
   calendarOpen.value = !calendarOpen.value;
 }
+
+function handleDateSelect(selectedDate) {
+  selectedDay.value = selectedDate;
+  currentMonth.value = selectedDate.getMonth();
+  currentYear.value = selectedDate.getFullYear();
+  week.value = getCurrentWeek(selectedDate);
+  calendarOpen.value = false;
+}
+
+const filteredTasks = computed(() => {
+  if (!selectedDay.value) return tasks.value;
+
+  const selected = new Date(selectedDay.value).toDateString();
+
+  return tasks.value.filter((task) => {
+    const [day, month, year] = task.date.split(".");
+    const taskDate = new Date(`${year}-${month}-${day}`);
+    return taskDate.toDateString() === selected;
+  });
+});
 </script>
 
 <style></style>
--- src/components/CalendarPicker.vue
+++ src/components/CalendarPicker.vue
@@ -2,10 +2,13 @@
   <v-container>
     <v-row justify="space-around">
       <v-date-picker
+        show-adjacent-months
         v-model="date"
         :allowed-dates="allowedDates"
         :max="formattedFutureDate"
         :min="formattedDate"
+        @update:modelValue="handleDateSelect"
+        :first-day-of-week="1"
       ></v-date-picker>
     </v-row>
   </v-container>
@@ -15,12 +18,22 @@
 import { useDate } from "vuetify";
 import { ref } from "vue";
 
+const emit = defineEmits(["date-selected"]);
+
 const date = ref(new Date());
 const adapter = useDate();
 const formattedDate = ref(adapter.toISO(date.value));
 const futureDate = new Date(date.value);
-
 futureDate.setMonth(futureDate.getMonth() + 6);
-
 const formattedFutureDate = ref(adapter.toISO(futureDate));
+
+function handleDateSelect(date) {
+  emit("date-selected", date);
+}
 </script>
+
+<style>
+body {
+  font-family: "Nunito", sans-serif;
+}
+</style>
